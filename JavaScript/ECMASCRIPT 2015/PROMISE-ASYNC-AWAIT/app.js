a59963d38f1ba277865aa0b4d513c075d292477d
FrekoJazz <jason_tirard6@hotmail.com>
2022-08-23 09:14:42 +0200

    Revert "Fin Promise, Async & Await"
    This reverts commit 394eed4ad467875d6fc69fb7189911075cf67cfa.

--- JavaScript/ECMASCRIPT 2015/PROMISE-ASYNC-AWAIT/app.js
+++ JavaScript/ECMASCRIPT 2015/PROMISE-ASYNC-AWAIT/app.js
@@ -42,6 +42,32 @@ let demo = async () => {
 
 demo()
 
+// EXEMPLE POUR L'EXPLICATIONS
+
+/* const getPosts = async () => {
+    try {
+        let response = await get('https://jsonplaceholder.typicode.com/usssssers')
+        let users = JSON.parse(response)
+        response = await get('https://jsonplaceholder.typicode.com/comments?userId=' + users[0].id)
+        let posts = JSON.parse(response)
+    } catch (e) {
+        console.log('il y a un probleme', e);
+    }
+    return posts
+}
+
+let getFirstPost = async () => {
+    let posts = await getPosts()
+    return posts[0]
+}
+
+let demo = async () => {
+    let arr = await Promise.all([getPosts(), getFirstPost()])
+    console.log(arr)
+}
+
+getPosts() */
+
 /**
  * On definie une méthode en mettant async devant => la méthode retourne une promesse quoi qu'il arrive
  * 
