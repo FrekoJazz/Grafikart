c120daec85a1d372399daec863a6b233ab8b28e5
FrekoJazz <jason_tirard6@hotmail.com>
2022-09-12 00:39:47 +0200

    Fin TP Créer un Carousel

--- JavaScript/CAS PRATRIQUES/Créer un Carousel/js/main.js
+++ JavaScript/CAS PRATRIQUES/Créer un Carousel/js/main.js
@@ -10,9 +10,9 @@ class Carousel {
     /**
      * @param {HTMLElement} element
      * @param {object} options
-     * @param {object} options.slideToScroll Nombre d'éléments à faire défiler
-     * @param {object} options.slideVisible Nombre d'éléments visible dans un slide
-     * @param {boolean} options.loop doit-on boucler en fin de carousel
+     * @param {object} [options.slideToScroll=1] Nombre d'éléments à faire défiler
+     * @param {object} [options.slideVisible=1] Nombre d'éléments visible dans un slide
+     * @param {boolean} [options.loop=false] doit-on boucler en fin de carousel
      */
 
     constructor(element, options = {}) {
@@ -22,13 +22,17 @@ class Carousel {
             slidesVisible: 1,
             loop: false
         }, options)
-        const children = [].slice.call(element.children)
+        let children = [].slice.call(element.children)
+        this.isMobile = false
         this.currentItem = 0 // C'est l'element qui est actuellement visible qui est sauvegardé
+        this.moveCallbacks = []
+
+        // Modification du DOM
         this.root = this.createDivWithClass('carousel')
         this.container = this.createDivWithClass('carousel__container')
+        this.root.setAttribute('tabindex', '0') // Tabindex permet de rendre un élément focusable. Si on lui passe en paramètres "0", il prend directement l'ordre HTML
         this.root.appendChild(this.container)
         this.element.appendChild(this.root)
-        this.moveCallbacks = []
         this.items = children.map(child => {
             let item = this.createDivWithClass('carousel__item')
             item.appendChild(child)
@@ -37,15 +41,27 @@ class Carousel {
         })
         this.setStyle()
         this.creatNavigation()
+
+        //Evènements
+        this.moveCallbacks.forEach(cb => cb(0)) // Masque nos éléments prev par default
+        this.onWindowResize()
+        window.addEventListener('resize', this.onWindowResize.bind(this))
+        this.root.addEventListener('keyup', e => {
+            if (e.key === 'ArrowRight') {
+                this.next()
+            } else if (e.key === 'ArrowLeft') {
+                this.prev()
+            }
+        })
     }
 
     /**
      * Applique les bonnes dimmensions aux élements du carousel
      */
     setStyle() {
-        let ratio = this.items.length / this.options.slidesVisible
+        let ratio = this.items.length / this.slidesVisible
         this.container.style.width = (ratio * 100) + "%"
-        this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%")
+        this.items.forEach(item => item.style.width = ((100 / this.slidesVisible) / ratio) + "%")
     }
 
     creatNavigation() {
@@ -55,8 +71,16 @@ class Carousel {
         this.root.appendChild(prevButton)
         nextButton.addEventListener('click', this.next.bind(this))
         prevButton.addEventListener('click', this.prev.bind(this))
+        if (this.options.loop === true) {
+            return
+        }
         this.onMove(index => {
             if (index === 0) {
+                prevButton.classList.add('carousel__prev--hidden')
+            } else {
+                prevButton.classList.remove('carousel__prev--hidden')
+            }
+            if (this.items[this.currentItem + this.slidesVisible] === undefined) {
                 nextButton.classList.add('carousel__next--hidden')
             } else {
                 nextButton.classList.remove('carousel__next--hidden')
@@ -79,9 +103,17 @@ class Carousel {
      */
     gotoItem(index) {
         if (index < 0) {
-            index = this.items.length - this.options.slidesVisible
-        } else if (index >= this.items.length || this.items[this.currentItem + this.options.slidesVisible] === undefined) {
-            index = 0
+            if (this.options.loop) {
+                index = this.items.length - this.slidesVisible
+            } else {
+                return
+            }
+        } else if (index >= this.items.length || (this.items[this.currentItem + this.slidesVisible] === undefined && index > this.currentItem)) {
+            if (this.options.loop) {
+                index = 0
+            } else {
+                return
+            }
         }
         let translateX = index * -100 / this.items.length
         this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)'
@@ -97,6 +129,15 @@ class Carousel {
         this.moveCallbacks.push(cb)
     }
 
+    onWindowResize() {
+        let mobile = window.innerWidth < 800
+        if (mobile !== this.isMobile) {
+            this.isMobile = mobile
+            this.setStyle()
+            this.moveCallbacks.forEach(cb => cb(this.currentItem))
+        }
+    }
+
 
     /**
      * @param {string} className
@@ -107,20 +148,40 @@ class Carousel {
         div.setAttribute('class', className)
         return div
     }
+
+    /**
+     * @returns {number}
+    */
+    get slideToScroll() {
+        return this.isMobile ? 1 : this.options.slidesToScroll
+    }
+
+    /**
+    * @returns {number}
+   */
+    get slidesVisible() {
+        return this.isMobile ? 1 : this.options.slidesVisible
+    }
 }
 
+function doSomething() {
+    new Carousel(document.querySelector('#carousel1'), {
+        slidesVisible: 3,
+        slidesToScroll: 2,
+        loop: true
+    })
 
-new Carousel(document.querySelector('#carousel1'), {
-    slidesVisible: 3,
-    slidesToScroll: 2,
-    loop: false
-})
+    new Carousel(document.querySelector('#carousel2'), {
+        slidesVisible: 2,
+        slidesToScroll: 2,
+    })
 
-new Carousel(document.querySelector('#carousel2'), {
-    slidesVisible: 2,
-    slidesToScroll: 2,
-})
+    new Carousel(document.querySelector('#carousel3'), {
 
-new Carousel(document.querySelector('#carousel3'), {
-
-})
+    })
+}
+if (document.readyState === 'loading') {  // Le chargement n'est pas encore terminé
+    document.addEventListener('DOMContentLoaded', doSomething);
+} else {  // `DOMContentLoaded` a déjà été déclenché
+    doSomething();
+}
