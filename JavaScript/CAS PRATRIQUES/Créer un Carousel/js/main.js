ccbf54da867684ebebd74746153438607cb5566e
FrekoJazz <jason_tirard6@hotmail.com>
2022-09-13 00:33:51 +0200

    Ajout du défilement infini au Carousel

--- JavaScript/CAS PRATRIQUES/Créer un Carousel/js/main.js
+++ JavaScript/CAS PRATRIQUES/Créer un Carousel/js/main.js
@@ -13,6 +13,7 @@ class Carousel {
      * @param {object} [options.slideToScroll=1] Nombre d'éléments à faire défiler
      * @param {object} [options.slideVisible=1] Nombre d'éléments visible dans un slide
      * @param {boolean} [options.loop=false] doit-on boucler en fin de carousel
+     * @param {boolean} [options.infinite=false]
      * @param {boolean} [options.pagination=false]
      * @param {boolean} [options.navigation=true]
      */
@@ -24,12 +25,14 @@ class Carousel {
             slidesVisible: 1,
             loop: false,
             pagination: false,
-            navigation: true
+            navigation: true,
+            infinite: false
         }, options)
         let children = [].slice.call(element.children)
         this.isMobile = false
         this.currentItem = 0 // C'est l'element qui est actuellement visible qui est sauvegardé
         this.moveCallbacks = []
+        this.offset = 0
 
         // Modification du DOM
         this.root = this.createDivWithClass('carousel')
@@ -40,9 +43,18 @@ class Carousel {
         this.items = children.map(child => {
             let item = this.createDivWithClass('carousel__item')
             item.appendChild(child)
-            this.container.appendChild(item)
             return item
         })
+        if (this.options.infinite) {
+            this.offset = this.options.slidesVisible * 1
+            this.items = [
+                ...this.items.slice(this.items.length - this.offset).map(item => item.cloneNode(true)),
+                ...this.items,
+                ...this.items.slice(0, this.offset).map(item => item.cloneNode(true))
+            ]
+            this.gotoItem(this.offset, false)
+        }
+        this.items.forEach(item => this.container.appendChild(item))
         this.setStyle()
         if (this.options.navigation) {
             this.creatNavigation()
@@ -52,7 +64,7 @@ class Carousel {
         }
 
         //Evènements
-        this.moveCallbacks.forEach(cb => cb(0)) // Masque nos éléments prev par default
+        this.moveCallbacks.forEach(cb => cb(this.currentItem)) // Masque nos éléments prev par default
         this.onWindowResize()
         window.addEventListener('resize', this.onWindowResize.bind(this))
         this.root.addEventListener('keyup', e => {
@@ -62,6 +74,9 @@ class Carousel {
                 this.prev()
             }
         })
+        if (this.options.infinite) {
+            this.container.addEventListener('transitionend', this.resetInfinite.bind(this))
+        }
     }
 
     /**
@@ -107,14 +122,15 @@ class Carousel {
         let pagination = this.createDivWithClass('carousel__pagination')
         let buttons = []
         this.root.appendChild(pagination)
-        for (let i = 0; i < this.items.length; i = i + this.options.slidesToScroll) {
+        for (let i = 0; i < (this.items.length - 2 * this.offset); i = i + this.options.slidesToScroll) {
             let button = this.createDivWithClass('carousel__pagination__button')
-            button.addEventListener('click', () => this.gotoItem(i))
+            button.addEventListener('click', () => this.gotoItem(i + this.offset))
             pagination.appendChild(button)
             buttons.push(button)
         }
         this.onMove(index => {
-            let activeButton = buttons[Math.floor(index / this.options.slidesToScroll)]
+            let count = this.items.length - 2 * this.offset
+            let activeButton = buttons[Math.floor(((index - this.offset) % count) / this.options.slidesToScroll)]
             if (activeButton) {
                 buttons.forEach(button => button.classList.remove('carousel__pagination__button--active'))
                 activeButton.classList.add('carousel__pagination__button--active')
@@ -134,8 +150,9 @@ class Carousel {
     /**
      * Déplace le carousel vers l'élement ciblé
      * @param {number} index
+     * @param {boolean} [animation =true]
      */
-    gotoItem(index) {
+    gotoItem(index, animation = true) {
         if (index < 0) {
             if (this.options.loop) {
                 index = this.items.length - this.slidesVisible
@@ -150,11 +167,28 @@ class Carousel {
             }
         }
         let translateX = index * -100 / this.items.length
+        if (animation === false) {
+            this.container.style.transition = 'none'
+        }
         this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)'
+        this.container.offsetHeight // force repaint
+        if (animation === false) {
+            this.container.style.transition = ''
+        }
         this.currentItem = index
         this.moveCallbacks.forEach(cb => cb(index))
     }
 
+    /**
+     * Deplacer le container pour donner l'impression d'un slide infinie
+     */
+    resetInfinite() {
+        if (this.currentItem <= this.options.slidesToScroll) {
+            this.gotoItem(this.currentItem + (this.items.length - 2 * this.offset), false)
+        } else if (this.currentItem >= this.items.length - this.offset) {
+            this.gotoItem(this.currentItem - (this.items.length - 2 * this.offset), false)
+        }
+    }
     /**
      * 
      * @param {moveCallback} cb 
@@ -201,7 +235,16 @@ class Carousel {
 function doSomething() {
     new Carousel(document.querySelector('#carousel1'), {
         slidesVisible: 3,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
+        loop: true,
+        pagination: true
+    })
+
+    new Carousel(document.querySelector('#carousel1__bis'), {
+        slidesVisible: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        pagination: true
     })
 
     new Carousel(document.querySelector('#carousel2'), {
